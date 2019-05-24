121920aec33ce16c2b56e17e9fbb4273d1f744a9
V8tr <neo_n@mail.ru>
2019-05-24 14:25:48 +0300

    Update sample data

--- CoreDataImages-ArticleTests/SampleData.swift
+++ CoreDataImages-ArticleTests/SampleData.swift
@@ -14,9 +14,13 @@ enum SampleData {
     static var images: [UIImage] = []
     
     static func initialize() {
-        images = stride(from: 0, to: 1_000_000, by: 1_000).map {
-            print($0)
-            UIImage.fromColor(.white, size: CGSize(width: $0, height: $0))
+        images = []
+        for size in stride(from: 0, to: 40_000, by: 1_000) {
+            autoreleasepool {
+                print(size)
+                let image = UIImage.fromColor(.white, size: CGSize(width: size, height: size))
+                images.append(image)
+            }
         }
     }
 }
